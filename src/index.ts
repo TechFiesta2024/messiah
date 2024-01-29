3dbccef2f1b3b9dced4762ef4cf5cbd7a9554a83
Sayan Mallick <lemon@Sayans-MacBook-Pro.local>
2024-01-29 11:00:41 +0530

    biomejs setup

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,37 @@
-import { Hono } from 'hono'
-import { workshops } from './types/workshops'
-import { events } from './types/events'
+import { Hono } from "hono";
+import { workshops } from "./types/workshops";
+import { events } from "./types/events";
 
-const app = new Hono()
+const app = new Hono();
 
-// TODO: implement testing
+// TODO
+// 1. user registration
+// 2. CI/CD pipeline
+// 3. testing
 
-const demoWorkshops = workshops
-const demoEvents = events
+const demoWorkshops = workshops;
+const demoEvents = events;
 
-app.get('/', (c) => {
-  return c.html('<h1>Bye World 🌎</h1>')
-})
+app.get("/", (c) => {
+	return c.html("<h1>Bye World 🌎</h1>");
+});
 
-app.get('/workshops', (c) => {
-  return c.json(demoWorkshops)
-})
+app.get("/workshops", (c) => {
+	return c.json(demoWorkshops);
+});
 
-app.get('/workshops/:id', (c) => {
-  const id = c.req.param('id')
-  return c.json(demoWorkshops.find((w) => w.id === Number(id)))
-})
+app.get("/workshops/:id", (c) => {
+	const id = c.req.param("id");
+	return c.json(demoWorkshops.find((w) => w.id === Number(id)));
+});
 
-app.get('/events', (c) => {
-  return c.json(demoEvents)
-})
+app.get("/events", (c) => {
+	return c.json(demoEvents);
+});
 
-app.get('/events/:id', (c) => {
-  const id = c.req.param('id')
-  return c.json(demoEvents.find((e) => e.id === Number(id)))
-})
+app.get("/events/:id", (c) => {
+	const id = c.req.param("id");
+	return c.json(demoEvents.find((e) => e.id === Number(id)));
+});
 
-export default app
+export default app;
--- src/types/users.ts
+++ src/types/users.ts
@@ -1,10 +1,10 @@
 export interface Users {
-  id: string; // user id, uuid
-  name: string; // name
-  email: string; // email
-  college_name: string; // college name
-  semester: number; // semester
-  contact: string; // contact number
-  workshops: [number]; // corresponding workshop id
-  events: [number]; // corresponding event id
+	id: string; // user id, uuid
+	name: string; // name
+	email: string; // email
+	college_name: string; // college name
+	semester: number; // semester
+	contact: string; // contact number
+	workshops: [number]; // corresponding workshop id
+	events: [number]; // corresponding event id
 }
