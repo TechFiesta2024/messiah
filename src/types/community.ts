aafbbe4035f8b47d00c03374bb1f62fd726ddd00
Sayan Mallick <lemon@Sayans-MacBook-Pro.local>
2024-02-01 14:57:40 +0530

    updated README

--- src/types/community.ts
+++ src/types/community.ts
@@ -1,8 +1,8 @@
-export interface Community {
+export interface community {
 	id: string; // user id, uuid
 	name: string; // name
 	email: string; // email
-	college_name: string; // college name
+	college: string; // college name
 	contact: string; // contact number
 	no_of_regs: number; // number of responses
 	socials: string[]; // social links
